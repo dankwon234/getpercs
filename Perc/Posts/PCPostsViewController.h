c6d4235e443ee0db3c9e0e775cbc37de2549b7bb
Dan Kwon <dankwon@Dans-MacBook-Air-2.local>
2015-04-18 01:42:15 -0400

    set up posts collection view

--- Perc/Posts/PCPostsViewController.h
+++ Perc/Posts/PCPostsViewController.h
@@ -4,10 +4,10 @@
 //
 //  Created by Dan Kwon on 4/16/15.
 //  Copyright (c) 2015 Perc. All rights reserved.
-//
+
 
 #import "PCViewController.h"
 
-@interface PCPostsViewController : PCViewController
+@interface PCPostsViewController : PCViewController <UICollectionViewDataSource, UICollectionViewDelegate>
 
 @end
--- Perc/Posts/PCPostsViewController.m
+++ Perc/Posts/PCPostsViewController.m
@@ -8,11 +8,16 @@
 
 #import "PCPostsViewController.h"
 #import "PCCreatePostViewController.h"
+#import "PCCollectionViewFlowLayout.h"
+#import "PCVenueCell.h"
 
 
 @interface PCPostsViewController ()
-
+@property (strong, nonatomic) UICollectionView *postsTable;
 @end
+
+static NSString *cellId = @"cellId";
+#define kTopInset 220.0f
 
 @implementation PCPostsViewController
 
@@ -40,15 +45,15 @@
     [super viewDidLoad];
     [self addCustomBackButton];
     
-//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Post"
-//                                                                              style:UIBarButtonItemStylePlain
-//                                                                             target:self
-//                                                                             action:nil];
-
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose
                                                                                            target:self
                                                                                            action:@selector(createPost:)];
 
+    if (self.currentZone.posts){
+        [self layoutListsCollectionView];
+        return;
+    }
+    
     [self.loadingIndicator startLoading];
     [[PCWebServices sharedInstance] fetchPostsInZone:self.currentZone.uniqueId completion:^(id result, NSError *error){
         [self.loadingIndicator stopLoading];
@@ -59,8 +64,38 @@
         
         NSDictionary *results = (NSDictionary *)result;
         NSLog(@"%@", [results description]);
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.currentZone.posts = [NSMutableArray array];
+            NSArray *a = results[@"posts"];
+            for (NSDictionary *postInfo in a)
+                [self.currentZone.posts addObject:[PCPost postWithInfo:postInfo]];
+            
+            [self layoutListsCollectionView];
+        });
+        
     }];
 }
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"imageData"]){
+        PCPost *post = (PCPost *)object;
+        [post removeObserver:self forKeyPath:@"imageData"];
+        
+        //this is smoother than a conventional reload. it doesn't stutter the UI:
+        dispatch_async(dispatch_get_main_queue(), ^{
+            int index = (int)[self.currentZone.posts indexOfObject:post];
+            PCVenueCell *cell = (PCVenueCell *)[self.postsTable cellForItemAtIndexPath:[NSIndexPath indexPathForRow:index inSection:0]];
+            
+            if (!cell)
+                return;
+            
+            cell.icon.image = post.imageData;
+        });
+    }
+}
+
 
 - (void)back:(UIGestureRecognizer *)swipe
 {
@@ -73,6 +108,128 @@
     PCCreatePostViewController *createPostVc = [[PCCreatePostViewController alloc] init];
     [self.navigationController pushViewController:createPostVc animated:YES];
 }
+
+
+- (void)layoutListsCollectionView
+{
+    if (self.postsTable){
+        [UIView animateWithDuration:0.40f
+                              delay:0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             CGRect frame = self.postsTable.frame;
+                             self.postsTable.frame = CGRectMake(frame.origin.x, self.view.frame.size.height, frame.size.width, frame.size.height);
+                             
+                         }
+                         completion:^(BOOL finished){
+//                             [self.postsTable removeObserver:self forKeyPath:@"contentOffset"];
+                             self.postsTable.delegate = nil;
+                             self.postsTable.dataSource = nil;
+                             [self.postsTable removeFromSuperview];
+                             self.postsTable = nil;
+                             [self layoutListsCollectionView];
+                         }];
+        
+        return;
+    }
+    
+    CGRect frame = self.view.frame;
+    
+    self.postsTable = [[UICollectionView alloc] initWithFrame:CGRectMake(0.0f, frame.size.height, frame.size.width, frame.size.height-20.0f) collectionViewLayout:[[PCCollectionViewFlowLayout alloc] init]];
+    self.postsTable.backgroundColor = [UIColor clearColor];
+    
+    [self.postsTable registerClass:[PCVenueCell class] forCellWithReuseIdentifier:cellId];
+    self.postsTable.contentInset = UIEdgeInsetsMake(kTopInset, 0.0f, 24.0f, 0);
+    self.postsTable.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight);
+    self.postsTable.dataSource = self;
+    self.postsTable.delegate = self;
+    self.postsTable.showsVerticalScrollIndicator = NO;
+//    [self.postsTable addObserver:self forKeyPath:@"contentOffset" options:0 context:nil];
+    [self.view addSubview:self.postsTable];
+    
+    [self refreshVenuesCollectionView];
+    
+    
+    [UIView animateWithDuration:1.20f
+                          delay:0
+         usingSpringWithDamping:0.6f
+          initialSpringVelocity:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         CGRect frame = self.postsTable.frame;
+                         self.postsTable.frame = CGRectMake(frame.origin.x, 0.0f, frame.size.width, frame.size.height);
+                         
+                     }
+                     completion:^(BOOL finished){
+                         
+                     }];
+}
+
+- (void)refreshVenuesCollectionView
+{
+    // IMPORTANT: Have to call this on main thread! Otherwise, data models in array might not be synced, and reload acts funky
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.postsTable.collectionViewLayout invalidateLayout];
+        [self.postsTable reloadData];
+        
+        NSMutableArray *indexPaths = [NSMutableArray array];
+        for (int i=0; i<self.currentZone.posts.count; i++)
+            [indexPaths addObject:[NSIndexPath indexPathForRow:i inSection:0]];
+        
+        [self.postsTable reloadItemsAtIndexPaths:indexPaths];
+    });
+}
+
+
+
+#pragma mark - UICollectionViewDataSource
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    //    NSLog(@"collectionView numberOfItemsInSection: %d", self.posts.count);
+    return self.currentZone.posts.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    PCVenueCell *cell = (PCVenueCell *)[collectionView dequeueReusableCellWithReuseIdentifier:cellId forIndexPath:indexPath];
+//    [cell.btnOrder addTarget:self action:@selector(viewVenue:) forControlEvents:UIControlEventTouchUpInside];
+    
+    PCPost *post = (PCPost *)self.currentZone.posts[indexPath.row];
+    cell.lblTitle.text = post.title;
+//    cell.lblLocation.text = [NSString stringWithFormat:@"%@, %@", [venue.city capitalizedString], [venue.state uppercaseString]];
+    cell.tag = indexPath.row+1000;
+    cell.btnOrder.tag = cell.tag;
+//    cell.lblDetails.text = [NSString stringWithFormat:@"Min Delivery Fee: $%d \u00b7 %.1f mi", venue.fee, venue.distance];
+    
+    if ([post.image isEqualToString:@"none"]){
+        cell.icon.image = [UIImage imageNamed:@"logo.png"];
+        return cell;
+    }
+    
+    if (post.imageData){
+        cell.icon.image = post.imageData;
+        return cell;
+    }
+    
+    cell.icon.image = [UIImage imageNamed:@"icon.png"];
+    [post addObserver:self forKeyPath:@"imageData" options:0 context:nil];
+    [post fetchImage];
+    return cell;
+}
+
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake([PCCollectionViewFlowLayout cellWidth], [PCCollectionViewFlowLayout cellHeight]);
+}
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
