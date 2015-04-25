7336a7942b0f2c6ca9b5cce319f88df1248117a0
Dan Kwon <dankwon@Dans-Air-2.home>
2015-04-25 19:23:17 -0400

    added reply button

--- Perc/Profile/PCMessageViewController.m
+++ Perc/Profile/PCMessageViewController.m
@@ -9,7 +9,9 @@
 #import "PCMessageViewController.h"
 
 @interface PCMessageViewController ()
-
+@property (strong, nonatomic) UIScrollView *theScrollview;
+@property (strong, nonatomic) UILabel *lblDate;
+@property (strong, nonatomic) UILabel *lblMessage;
 @end
 
 @implementation PCMessageViewController
@@ -20,9 +22,75 @@
 {
     UIView *view = [self baseView];
     view.backgroundColor = kLightGray;
-//    CGRect frame = view.frame;
+    CGRect frame = view.frame;
+    
+    self.theScrollview = [[UIScrollView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, frame.size.width, frame.size.height)];
+    
+    
+    CGFloat h = 44.0f;
+    CGFloat y = 0.0;
+    
+    UILabel *lblSource = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, y, frame.size.width, h)];
+    lblSource.backgroundColor = [UIColor whiteColor];
+    lblSource.text = [NSString stringWithFormat:@"  FROM: %@ %@", [self.message.profile.firstName capitalizedString], [self.message.profile.lastName capitalizedString]];
+    lblSource.font = [UIFont fontWithName:kBaseFontName size:16.0f];
+    [self.theScrollview addSubview:lblSource];
+    y += h+1.0f;
 
     
+    UILabel *lblRecipient = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, y, frame.size.width, h)];
+    lblRecipient.backgroundColor = [UIColor whiteColor];
+    lblRecipient.text = [NSString stringWithFormat:@"  TO: %@ %@", [self.message.recipient.firstName capitalizedString], [self.message.recipient.lastName capitalizedString]];
+    lblRecipient.font = [UIFont fontWithName:kBaseFontName size:16.0f];
+    [self.theScrollview addSubview:lblRecipient];
+    y += h+12.0f;
+    
+    CGFloat x = 20.0f;
+    self.lblDate = [[UILabel alloc] initWithFrame:CGRectMake(x, y, frame.size.width, 18.0f)];
+    self.lblDate.textColor = kOrange;
+    self.lblDate.font = [UIFont boldSystemFontOfSize:12.0f];
+    self.lblDate.text = self.message.formattedDate;
+    [self.theScrollview addSubview:self.lblDate];
+    y += self.lblDate.frame.size.height+12.0f;
+    
+    CGFloat width = frame.size.width-2*x;
+    UIFont *font = [UIFont fontWithName:kBaseFontName size:16.0f];
+    CGRect bounds = [self.message.content boundingRectWithSize:CGSizeMake(width, 1000.0f)
+                                                       options:NSStringDrawingUsesLineFragmentOrigin
+                                                    attributes:@{NSFontAttributeName : font}
+                                                       context:nil];
+    
+    self.lblMessage = [[UILabel alloc] initWithFrame:CGRectMake(x, y, width, bounds.size.height)];
+    self.lblMessage.numberOfLines = 0;
+    self.lblMessage.lineBreakMode = NSLineBreakByWordWrapping;
+    self.lblMessage.font = font;
+    self.lblMessage.textColor = kLightBlue;
+    self.lblMessage.text = self.message.content;
+    [self.theScrollview addSubview:self.lblMessage];
+    y += self.lblMessage.frame.size.height+44.0f;
+
+    
+    self.theScrollview.contentSize = CGSizeMake(0, y);
+    [view addSubview:self.theScrollview];
+    
+    
+    UIView *bgReply = [[UIView alloc] initWithFrame:CGRectMake(0.0f, frame.size.height-84.0f, frame.size.width, 64.0f)];
+    bgReply.backgroundColor = [UIColor grayColor];
+    bgReply.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    
+    UIButton *btnReply = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnReply.frame = CGRectMake(12.0f, 12.0f, frame.size.width-24.0f, h);
+    btnReply.layer.borderColor = [[UIColor whiteColor] CGColor];
+    btnReply.layer.borderWidth = 1.0f;
+    btnReply.layer.cornerRadius = 0.5f*h;
+    btnReply.layer.masksToBounds = YES;
+    [btnReply setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [btnReply setTitle:@"Reply" forState:UIControlStateNormal];
+    btnReply.titleLabel.font = [UIFont fontWithName:kBaseFontName size:18.0f];
+    [btnReply addTarget:self action:@selector(reply) forControlEvents:UIControlEventTouchUpInside];
+    [bgReply addSubview:btnReply];
+    
+    [view addSubview:bgReply];
     
     
     UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(back:)];
@@ -37,6 +105,12 @@
     [super viewDidLoad];
     [self addCustomBackButton];
 }
+
+- (void)reply
+{
+    NSLog(@"reply");
+}
+
 
 - (void)didReceiveMemoryWarning
 {
