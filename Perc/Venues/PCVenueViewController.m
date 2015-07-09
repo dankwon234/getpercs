848600e597f3201071613140d4817c34191dec69
Dan Kwon <dankwon@Dans-Air-2.home>
2015-07-09 02:47:26 -0400

    added address and city labels

--- Perc/Venues/PCVenueViewController.m
+++ Perc/Venues/PCVenueViewController.m
@@ -65,8 +65,8 @@
     
     
     
-    static CGFloat dimen = 120.0f;
-    y = 40.0f;
+    static CGFloat dimen = 140.0f;
+    y = 36.0f;
     UIImageView *venueIcon = [[UIImageView alloc] initWithFrame:CGRectMake(16.0f, y, dimen, dimen)];
     venueIcon.image = venueImage;
     venueIcon.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
@@ -76,17 +76,52 @@
     venueIcon.layer.shadowRadius = 2.0f;
     venueIcon.layer.shadowPath = [UIBezierPath bezierPathWithRect:venueIcon.bounds].CGPath;
     [view addSubview:venueIcon];
+    y += 4.0f;
     
     CGFloat x = venueIcon.frame.origin.x+dimen+12.0f;
-    UILabel *lblVenueName = [[UILabel alloc] initWithFrame:CGRectMake(x, y, frame.size.width-x, 18.0f)];
-    lblVenueName.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    CGFloat width = frame.size.width-x;
+    UIFont *bold = [UIFont boldSystemFontOfSize:18.0f];
+    
+    bounds = [self.venue.name boundingRectWithSize:CGSizeMake(width, 100.0f)
+                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                        attributes:@{NSFontAttributeName:bold}
+                                           context:nil];
+
+    
+    UILabel *lblVenueName = [[UILabel alloc] initWithFrame:CGRectMake(x, y, width, bounds.size.height)];
+    lblVenueName.autoresizingMask = (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleHeight);
     lblVenueName.numberOfLines = 0;
     lblVenueName.lineBreakMode = NSLineBreakByWordWrapping;
     lblVenueName.textColor = [UIColor whiteColor];
     lblVenueName.text = self.venue.name;
-    lblVenueName.font = [UIFont boldSystemFontOfSize:18.0f];
+    lblVenueName.font = bold;
     [view addSubview:lblVenueName];
+    y += lblVenueName.frame.size.height+8.0f;
     
+    UILabel *lblVenueAddress = [[UILabel alloc] initWithFrame:CGRectMake(x, y, width, 16.0f)];
+    lblVenueAddress.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    lblVenueAddress.textColor = [UIColor whiteColor];
+    lblVenueAddress.text = [self.venue.address capitalizedString];
+    lblVenueAddress.font = [UIFont systemFontOfSize:14.0f];
+    [view addSubview:lblVenueAddress];
+    y += lblVenueAddress.frame.size.height;
+
+    
+    UILabel *lblVenueTown = [[UILabel alloc] initWithFrame:CGRectMake(x, y, width, 16.0f)];
+    lblVenueTown.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    lblVenueTown.textColor = lblVenueAddress.textColor;
+    lblVenueTown.text = [NSString stringWithFormat:@"%@, %@", [self.venue.city capitalizedString], [self.venue.state uppercaseString]];
+    lblVenueTown.font = lblVenueAddress.font;
+    [view addSubview:lblVenueTown];
+    y += lblVenueTown.frame.size.height;
+
+//    UILabel *lblVenuePhone = [[UILabel alloc] initWithFrame:CGRectMake(x, y, width, 16.0f)];
+//    lblVenuePhone.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+//    lblVenuePhone.textColor = lblVenueAddress.textColor;
+//    lblVenuePhone.text = @"203-722-7160";
+//    lblVenuePhone.font = lblVenueAddress.font;
+//    [view addSubview:lblVenuePhone];
+//    y += lblVenuePhone.frame.size.height;
     
     
     
